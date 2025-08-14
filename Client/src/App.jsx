128b3d9a504098bf096eba7b4def31ba84b558e2
BIMAL6202 <bimalraj6202@gmail.com>
2025-08-14 10:19:56 +0530

    app.jsx updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,7 @@ import QuizPage from './components/quizPage'
 import Login from './components/login'
 import SignUp from './components/signup'
 import QuizzBoard from './components/quizzBoard'
-import LandingPage from './components/landingPage'
+import LandingPage from './components/LandingPage'
 import { Route, Routes } from 'react-router-dom'
 import Results from './components/results'
 
